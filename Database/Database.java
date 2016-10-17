497cd0446c76f9f9e8be7b98c0e8d5e38ae6d3fc
BleedingEdgeAI <christianrmiles@gmail.com>
2016-10-18 00:36:50 +1000

    Added item functionality (The item database)

--- Database/Database.java
+++ Database/Database.java
@@ -1,27 +1,128 @@
 import java.io.*;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Database {
     private File database;
+    private File menuDB;
     
     //Loads the database or builds a new one if it doesn't exist
     public Database(){
-        database = new File("database.sv");
+        database = new File("DB.sv");
+        menuDB = new File("menuDB.sv");
         
         if(!database.exists()){
             
             try {
                 database.createNewFile(); //Create a new DB if one doesn't already exist
+                menuDB.createNewFile();
             } catch (IOException e){}
             
+        }
+    }
+    
+    //Adds a new item to the menu
+    public void addItem(String name, int price){
+        int size = getMenuSize();
+        
+        try {
+            PrintWriter output = new PrintWriter(new FileWriter(menuDB,true)); //Opens a new PrintWriter in append mode
+            output.println(size);
+            output.println(name);
+            output.println(price);
+            output.close(); //Closes the PrintWriter
+        } catch (FileNotFoundException e){} catch (IOException e){}
+            
+    }
+    //Gets an item from the DB
+    public Item getItem(int index){
+        try {
+            Scanner in = new Scanner(menuDB);
+            int currIndex;
+            
+            while(in.hasNextLine()){
+                currIndex = Integer.parseInt(in.nextLine());
+                
+                if(currIndex == index){
+                    Item found = new Item(in.nextLine(), Integer.parseInt(in.nextLine()));
+                    return found;
+                } else {
+                    //Ignore data
+                    in.nextLine();
+                    in.nextLine();
+                }
+            }
+        } catch(FileNotFoundException e){}
+        
+        return null; //If the item doesn't exist, return null
+    }
+    //Changes a menu item
+    public void updatePrice(int index, int newPrice){
+        File temp = new File("update.temp");
+        
+        try{
+            PrintWriter output = new PrintWriter(temp); 
+            Scanner in = new Scanner(menuDB); //Read from the menuDB
+            String update; //The price to update
+            int nextNum;
+            
+            temp.createNewFile(); //Create the temp file for storage
+            
+            while(in.hasNextLine()){
+                nextNum = Integer.parseInt(in.nextLine());
+                output.println(nextNum);
+                output.println(in.nextLine());
+                update = in.nextLine();
+                
+                if(nextNum == index){
+                    update = ""+newPrice;
+                }
+                
+                output.println(update);
+                
+            }
+            
+            output.close();
+            output = new PrintWriter(menuDB);
+            in = new Scanner(temp);
+            
+            while(in.hasNextLine()){
+                output.println(in.nextLine());
+            }
+            output.close();
+            temp.delete(); //Delete the temporary file
+        } catch(IOException e){}
+    }
+    //Removes a menu item
+    public void removeItem(int index){
+        
+    }
+    //Gets the size of the menuDB
+    public int getMenuSize(){
+        int size = 0;
+        
+        try{
+            Scanner in = new Scanner(menuDB);
+            while(in.hasNextLine()){
+                //Throw away
+                in.nextLine();
+                in.nextLine();
+                in.nextLine();
+                
+                size++;
+            }
+            
+            return size;
+        } catch (FileNotFoundException e){
+            return 0;
         }
     }
     //Returns a customer if they exist, otherwise returns a null value
     public Customer getCustomer(String number){
         try{
+            
             Scanner scan = new Scanner(database);
             boolean scanResult = false;
+            
             while(scan.hasNextLine()){
                 if(scan.hasNext(number)){
                     scanResult = true;
@@ -35,6 +136,7 @@ public class Database {
             } else {
                 return null;
             }
+            
         } catch (FileNotFoundException e){
             return null;
         }
@@ -54,7 +156,7 @@ public class Database {
         } catch (FileNotFoundException e){} catch (IOException e){}
         
     }
-    //Gets the current size of the database
+    //Gets the current size of the customer database
     public int getSize(){
         try {
             Scanner scan = new Scanner(database);
@@ -68,4 +170,5 @@ public class Database {
             return 0;
         }
     }
+    
 }
--- Database/Readme.txt
+++ Database/Readme.txt
@@ -1,5 +1,13 @@
 Functions are as listed below:
 
+addItem(String name, int price) //Adds an item to the item menu
+
+getItem(int index) // Gets an item at the specified index
+
+updatePrice(int index, int newPrice) //Updates the item price at the given index
+
+removeItem(int index) //to be added
+
 getCustomer(String number) //Finds the specified customer and returns a customer object
 
 addCustomer(String number, String address, String financial) //Returns a customer object
