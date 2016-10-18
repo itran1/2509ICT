3e445abdb4b99536b6240cfd4c406490e0534ced
seandoran <sean_doran@outlook.com>
2016-10-18 23:53:30 +1000

    Order class

---
+++ Database/Order.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Order{
+public Map<Item,Integer> map = new HashMap<Item,Integer>();
+public Database DB = new Database();
+
+public void addOrder(int i, int q){
+	map.put(DB.getItem(i),q);
+}
+
+public void removeOrder(Item i){
+	map.remove(i);
+}
+public int getQuantity(Item i){
+	return 	map.get(i);
+}
+
+public void clearOrder(){
+	map.clear();
+}
+}
