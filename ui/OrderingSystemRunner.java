30443098765e749cb5ade06f7b904c72648eceed
David Moschner <david.moschner@griffithuni.edu.au>
2016-10-20 07:56:24 +1000

    Added sample menu. Use the sample argument when running to generate sample data for the menu.

--- ui/OrderingSystemRunner.java
+++ ui/OrderingSystemRunner.java
@@ -3,6 +3,12 @@ package ui;
 public class OrderingSystemRunner {
 
 	public static void main(String[] args) {
+		if(args.length >= 1) {
+			if(args[0].equals("sample")) {
+				System.out.println(args[0]);
+				SampleMenu.loadSampleData();
+			}
+		}
 		new UIController();
 
 	}
